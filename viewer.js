397bd2a70c2c79ba244cefa9e6310ffa30947590
Cgines-stech <cgines@stech.edu>
2025-05-23 12:40:17 -0600

    update

--- viewer.js
+++ viewer.js
@@ -1,33 +1,27 @@
 /*viewer.js*/
 function setFile(pdfPath, keyForChecklist) {
-  saveChecklistItem();
+  // First, determine the index of the file to save before changing it
+  const previousFileIndex = currentFileIndex;
+  const previousFileKey = flatFileList[previousFileIndex];
+
+  // Save checklist using the old file key
+  const selectedStatus = checklistContainer.querySelector('input[name="status"]:checked')?.value || "";
+  const comment = checklistContainer.querySelector('textarea[name="comment"]')?.value || "";
+  const allData = JSON.parse(localStorage.getItem("checklist") || "{}");
+
+  if (previousFileKey) {
+    const itemKey = checklistItems[currentItem];
+    if (!allData[previousFileKey]) allData[previousFileKey] = {};
+    allData[previousFileKey][itemKey] = { status: selectedStatus, comment };
+    localStorage.setItem("checklist", JSON.stringify(allData));
+  }
+
+  // Then update the index and load the new checklist
   currentFileIndex = flatFileList.indexOf(keyForChecklist);
   currentItem = 0;
-  viewerEl.src = `${pdfPath}?t=${Date.now()}`; // force reload
-  checklistContainer.innerHTML = ""; // safeguard clear
+
+  viewerEl.src = `${pdfPath}?t=${Date.now()}`;
+  checklistContainer.innerHTML = ""; // clear out UI
   loadChecklist();
-}
-
-function prevFile() {
-  if (currentFileIndex > 0) {
-    saveChecklistItem();
-    currentFileIndex--;
-    currentItem = 0;
-
-    const key = flatFileList[currentFileIndex];
-    const url = `https://cgines-stech.github.io/sitevisit/pdf/${key}`;
-    setFile(url, key);
-  }
-}
-
-function nextFile() {
-  if (currentFileIndex < flatFileList.length - 1) {
-    saveChecklistItem();
-    currentFileIndex++;
-    currentItem = 0;
-
-    const key = flatFileList[currentFileIndex];
-    const url = `https://cgines-stech.github.io/sitevisit/pdf/${key}`;
-    setFile(url, key);
-  }
+  highlightCurrentFile(keyForChecklist); // optional
 }
